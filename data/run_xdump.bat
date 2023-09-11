e57b4f5de0cee744695b891990d0dc05e213061c
Sebastian Werhausen <swerhausen@gmail.com>
2023-09-11 00:00:00 +0000

    well that's a new one: powershell file output for some reason writes a byte-reversed BOM

---
+++ data/run_xdump.bat
@@ -0,0 +1,6 @@
+S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:STDT -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_stars.txt 
+S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:PNDT -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_planets.txt
+S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:BIOM -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_biomes.txt
+S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:RSGD -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_resource_generation.txt
+S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:IRES -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_resources.txt
+S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:FLOR -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_flora.txt
--- data/run_xdump.ps1
+++
@@ -1,5 +0,0 @@
-& S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:STDT -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_stars.txt 
-& S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:PNDT -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_planets.txt
-& S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:BIOM -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_biomes.txt
-& S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:RSGD -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_resource_generation.txt
-& S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:IRES -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_resources.txt
