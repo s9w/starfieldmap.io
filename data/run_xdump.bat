7b7a559842f08717f36d8d5ba0f8f7c75a16982d
Sebastian Werhausen <swerhausen@gmail.com>
2023-09-18 00:00:00 +0000

    expanded data collection

--- data/run_xdump.bat
+++ data/run_xdump.bat
@@ -4,3 +4,6 @@ S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:BIOM -xr:TNAM,PCCC
 S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:RSGD -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_resource_generation.txt
 S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:IRES -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_resources.txt
 S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:FLOR -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_flora.txt
+S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:WEAP -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_weapons.txt
+S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:OMOD -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_omod.txt
+S:\SteamLibrary\steamapps\common\Starfield\xDump.exe -SF1 -dg:LCTN -xr:TNAM,PCCC S:\SteamLibrary\steamapps\common\Starfield\Data\starfield.esm > xdump_lctn.txt
